3e68393e007855749154155acb7cf710cc9d91a1
tourusa <72606766+tourusa@users.noreply.github.com>
2023-04-15 16:27:58 +0800

    Update Vercel.js

--- components/Vercel.js
+++ components/Vercel.js
@@ -1,5 +1,13 @@
 const Vercel = () => {
   return (
+    <a
+      href="https://github.com/craigary/nobelium"
+      target="_blank"
+      rel="noreferrer"
+      aria-label="noblium"
+    >
+      powered by noblium
+    </a>
   )
 }
 
